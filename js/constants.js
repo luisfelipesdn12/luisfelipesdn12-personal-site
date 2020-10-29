5994903255dbad21d9360e200cf6b6a63dbf1e68
unknown <luisfelipesdn12@gmail.com>
2020-10-29 17:34:50 -0300

    Add dynamic languages

---
+++ js/constants.js
@@ -0,0 +1,33 @@
+export const USERNAME = "luisfelipesdn12";
+export const LANGUAGES = [
+    {
+        name : "Python",
+        iDoWith : "Automation, data science, graphic interface.",
+        image : "../assets/python.png"  
+    },
+    {
+        name : "Java",
+        iDoWith : "Backend development, REST APIs.",
+        image : "../assets/java.png"
+    },
+    {
+        name : "Go",
+        iDoWith : "Servers, automation, backend development, REST APIs.",
+        image : "../assets/golang.png"
+    },
+    {
+        name : "HTML",
+        iDoWith : "Hot sites, iframe integrations, CSS libraries.",
+        image : "../assets/html-5.png"
+    },
+    {
+        name : "JavaScript",
+        iDoWith : "API consume, dynamic content.",
+        image : "../assets/javascript.png"
+    },
+    {
+        name : "C/C++",
+        iDoWith : "Competitive programming, Arduino, embedded systems.",
+        image : "../assets/c-plus-plus-logo.png"
+    }
+];
---
+++ js/utils.js
@@ -0,0 +1,54 @@
+import { USERNAME, LANGUAGES } from './constants.js';
+
+export function getGithubLanguageSearchURL(language) {
+    return "https://github.com/search?l=" + language+ "&q=user%3A" + USERNAME;
+}
+
+export function addToLanguagesList(language) {
+    const languagesList = document.getElementById("languages-list");
+
+    const card = document.createElement("li");
+    card.className = "rounded bg-indigo-100 bg-opacity-0 overflow-hidden shadow p-3 mb-3 md:w-1/2";
+    card.style = "margin-right: -5px; margin-left: -5px;";
+    languagesList.appendChild(card);
+
+    const cardLink = document.createElement("a");
+    cardLink.href = getGithubLanguageSearchURL(language.name);
+    cardLink.target = "_blank";
+    card.appendChild(cardLink);
+
+    const imageAndName = document.createElement("div");
+    imageAndName.className = "flex";
+    cardLink.appendChild(imageAndName);
+
+    const image = document.createElement("img");
+    image.src = language.image;
+    image.className = "rounded-full w-10 mr-6 mb-3 shadow p-1";
+    imageAndName.appendChild(image);
+
+    const name = document.createElement("p");
+    name.innerHTML = language.name;
+    name.className = "text-gray-700 font-semibold";
+    imageAndName.appendChild(name);
+
+    const iDoWith = document.createElement("p");
+    iDoWith.innerHTML = language.iDoWith;
+    iDoWith.className = "text-gray-700";
+    cardLink.appendChild(iDoWith);
+}
+
+export function hideMockInLanguagesList() {
+    const mockCards = document.getElementsByClassName("mock-language-card");
+
+    for (let i = 0; i < mockCards.length; i++) {
+        mockCards[i].className = mockCards[i].className + " hidden";
+    }
+}
+
+export function fillLanguageList() {
+    for (let i = 0; i < LANGUAGES.length; i++) {
+        addToLanguagesList(LANGUAGES[i]);
+    }
+
+    setInterval(hideMockInLanguagesList, 100);
+}
