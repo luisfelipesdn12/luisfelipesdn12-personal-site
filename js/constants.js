13396c5975553ad3ce3e67b4e4967d6c2654cecc
unknown <luisfelipesdn12@gmail.com>
2020-10-30 10:01:12 -0300

    Add dynamic content in the whole page

--- js/constants.js
+++ js/constants.js
@@ -1,33 +1,41 @@
 export const USERNAME = "luisfelipesdn12";
-export const LANGUAGES = [
-    {
-        name : "Python",
-        iDoWith : "Automation, data science, graphic interface.",
-        image : "../assets/python.png"  
-    },
-    {
-        name : "Java",
-        iDoWith : "Backend development, REST APIs.",
-        image : "../assets/java.png"
-    },
-    {
-        name : "Go",
-        iDoWith : "Servers, automation, backend development, REST APIs.",
-        image : "../assets/golang.png"
-    },
-    {
-        name : "HTML",
-        iDoWith : "Hot sites, iframe integrations, CSS libraries.",
-        image : "../assets/html-5.png"
-    },
-    {
-        name : "JavaScript",
-        iDoWith : "API consume, dynamic content.",
-        image : "../assets/javascript.png"
-    },
-    {
-        name : "C/C++",
-        iDoWith : "Competitive programming, Arduino, embedded systems.",
-        image : "../assets/c-plus-plus-logo.png"
-    }
-];
+export const HERO = {
+    greeting : "Hello, I'm Luis Felipe!",
+    sentence : 'I\'m a computer science student and I can code for: automations, websites, machine learning, APIs and data science. My main languages are <span class="text-blue-700 font-semibold">Python</span>, <span class="text-blue-700 font-semibold">Java</span> and <span class="text-blue-700 font-semibold">Go</span>.'
+};
+export const LANGUAGES = {
+    title : "Languages",
+    description : "Click in the language to see my repositories with it.",
+    unorderedList : [
+        {
+            name : "Python",
+            iDoWith : "Automation, data science, graphic interface.",
+            image : "../assets/python.png"  
+        },
+        {
+            name : "Java",
+            iDoWith : "Backend development, REST APIs.",
+            image : "../assets/java.png"
+        },
+        {
+            name : "Go",
+            iDoWith : "Servers, automation, backend development, REST APIs.",
+            image : "../assets/golang.png"
+        },
+        {
+            name : "HTML",
+            iDoWith : "Web sites, integrations with CSS libraries.",
+            image : "../assets/html-5.png"
+        },
+        {
+            name : "JavaScript",
+            iDoWith : "API consume, dynamic content in HTML.",
+            image : "../assets/javascript.png"
+        },
+        {
+            name : "C/C++",
+            iDoWith : "Competitive programming, Arduino, embedded systems.",
+            image : "../assets/c-plus-plus-logo.png"
+        }
+    ]
+};
